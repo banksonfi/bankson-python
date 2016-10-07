7796c8a106bed28dd8d20c9a87cd3c0a9e08826b
Niklas Närhinen <niklas@narhinen.net>
2016-10-07 04:26:31 +0300

    Support inbound payment params

--- bankson/inboundpayments.py
+++ bankson/inboundpayments.py
@@ -2,8 +2,8 @@ class InboundPayments(object):
     def __init__(self, base):
         self.base = base
 
-    def list(self):
-        return self.base.get('/inboundpayments')
+    def list(self, params={}):
+        return self.base.get('/inboundpayments', params=params)
 
     def refresh(self, certificate_id):
         return self.base.post('/inboundpayments', json={ 'certificate_id': certificate_id })
